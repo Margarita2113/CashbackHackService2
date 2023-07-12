aa9949aa7a05187613623ea69ebf171738ab4d73
Oskina Margarita <rita_2113@mail.ru>
2023-07-12 17:46:00 +0300

    Tests use JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,57 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+
+
+    @org.junit.Test
+    public void shouldRemainsIfBuy600() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(600);
+        int expected = 400;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldRemainsIfBuy999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldRemainsIfBuy1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldRemainsIfBuy1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldRemainsIfNotBuy() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+
+}
